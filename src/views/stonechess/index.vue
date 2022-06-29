1721030da4f2f6d3934e07347467225b744b2960
李延送 <liyansong@longavityfor.com>
2022-06-29 15:56:00 +0800

    石头棋初始化

--- src/views/stonechess/index.vue
+++ src/views/stonechess/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 李延送
  * @Date: 2022-06-29 15:44:41
- * @LastEditTime: 2022-06-29 15:49:15
+ * @LastEditTime: 2022-06-29 15:55:54
  * @Description: Do not edit
 -->
 <template>
@@ -28,4 +28,18 @@ export default class extends Vue {}
 </script>
 
 <style scoped lang="scss">
+.content {
+    .title {
+        margin-bottom: 20px;
+        font-size: 25px;
+        color: #000;
+        font-weight: bold;
+        text-shadow: 0 1px 7px rgb(124, 123, 123);
+    }
+    .mycanvas {
+        background: rgb(238, 164, 90);
+        border-radius: 5px;
+        box-shadow: 0 5px 10px #333;
+    }
+}
 </style>
