520555cceff9f65caeb75a553a8b7b5abe3c7f44
李延送 <liyansong@longavityfor.com>
2022-06-29 15:49:20 +0800

    石头棋初始化

--- src/views/stonechess/index.vue
+++ src/views/stonechess/index.vue
@@ -1,13 +1,19 @@
 <!--
  * @Author: 李延送
  * @Date: 2022-06-29 15:44:41
- * @LastEditTime: 2022-06-29 15:45:48
+ * @LastEditTime: 2022-06-29 15:49:15
  * @Description: Do not edit
 -->
 <template>
     <div class="content">
         <div class="title">
             石 头 棋
+        </div>
+        <div class="main">
+            <canvas class="mycanvas"
+                    ref="mycanvas"
+                    width="800"
+                    height="800"></canvas>
         </div>
     </div>
 </template>
