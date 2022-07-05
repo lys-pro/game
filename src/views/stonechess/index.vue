982ccb4aa10864612978cf52bf29203a48272439
lyspro <2540376566@qq.com>
2022-07-05 15:37:45 +0800

    init

--- src/views/stonechess/index.vue
+++ src/views/stonechess/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 李延送
  * @Date: 2022-06-29 15:44:41
- * @LastEditTime: 2022-06-29 15:55:54
+ * @LastEditTime: 2022-07-01 16:18:39
  * @Description: Do not edit
 -->
 <template>
@@ -25,11 +25,82 @@ import { Component, Vue } from "vue-property-decorator";
     name: "stonechess"
 })
 export default class extends Vue {
-    private muCanv:any = {}
-    private ctx:any = {}
+    private myCanv: any = {};
+    private ctx: any = {};
+    private step = 0;
+    private level: any = [
+        // 棋自布局 1 子棋 2 母棋
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 2, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1]
+    ];
+    private fiveChess: any = {
+        while: { name: "白棋", color: "#fff", data: [] },
+        black: { name: "黑棋", color: "#333", data: [] }
+    };
+    private chees = 0; // 当前执棋
 
     // 初始化
-    init() {}
+    init() {
+        this.myCanv = this.$refs.mycanvas;
+        this.ctx = this.myCanv.getContext("2d");
+        this.step = 700 / 4;
+
+        this.drawBoard();
+    }
+
+    // 生成棋盘
+    private drawBoard() {
+        for (var i = 0; i < 5; i++) {
+            this.drawLine(
+                { x: 50 + i * this.step, y: 50 },
+                { x: 50 + i * this.step, y: 750 }
+            );
+            this.drawLine(
+                { x: 50, y: 50 + i * this.step },
+                { x: 750, y: 50 + i * this.step }
+            );
+        }
+
+        this.drawLine({ x: 50, y: 50 }, { x: 750, y: 750 });
+        this.drawLine({ x: 50, y: 750 }, { x: 750, y: 50 });
+        this.drawLine({ x: 50, y: 400 }, { x: 400, y: 50 });
+        this.drawLine({ x: 50, y: 400 }, { x: 400, y: 750 });
+        this.drawLine({ x: 400, y: 50 }, { x: 750, y: 400 });
+        this.drawLine({ x: 400, y: 750 }, { x: 750, y: 400 });
+
+        this.drawChess(2, 2, "#333", 10);
+        this.drawChess(1, 1, "#333", 10);
+        this.drawChess(3, 1, "#333", 10);
+        this.drawChess(1, 3, "#333", 10);
+        this.drawChess(3, 3, "#333", 10);
+    }
+
+    // 画线
+    private drawLine(m: any, l: any) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(m.x, m.y);
+        this.ctx.lineTo(l.x, l.y);
+        this.ctx.stroke();
+    }
+
+    // 生成棋子
+    private drawChess(x: any, y: any, c: any, r: any) {
+        let w = 50 + x * this.step;
+        let h = 50 + y * this.step;
+
+        this.ctx.beginPath();
+        this.ctx.arc(w, h, r, 0, 2 * Math.PI);
+        this.ctx.fillStyle = c;
+        this.ctx.fill();
+        this.ctx.stroke();
+    }
+
+    mounted() {
+        this.init();
+    }
 }
 </script>
 
